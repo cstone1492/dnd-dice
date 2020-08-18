c2e80a8a84a8e51dd74ea9dbefa482012160b9b0
Caroline STone <docphilstone@gmail.com>
2020-08-18 13:49:02 -0500

    created basic file structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,6 +9,10 @@ import { UserDataComponent } from './user-data/user-data.component';
 import { FormComponent } from './form/form.component';
 import { ChiSquareFormComponent } from './chi-square-form/chi-square-form.component';
 import { CreateDieSetComponent } from './form/create-die-set/create-die-set.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ServicesComponent } from './services/services.component';
 
 @NgModule({
   declarations: [
@@ -16,7 +20,11 @@ import { CreateDieSetComponent } from './form/create-die-set/create-die-set.comp
     UserDataComponent,
     FormComponent,
     ChiSquareFormComponent,
-    CreateDieSetComponent
+    CreateDieSetComponent,
+    HomeComponent,
+    LoginComponent,
+    RegisterComponent,
+    ServicesComponent
   ],
   imports: [
     BrowserModule,
