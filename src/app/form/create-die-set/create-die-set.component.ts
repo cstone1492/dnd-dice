00e0e6fc7320804e9ce8abc9844b5a6abbc1ce57
Caroline STone <docphilstone@gmail.com>
2020-08-30 09:36:00 -0500

    initial create-die-set form

--- src/app/form/create-die-set/create-die-set.component.ts
+++ src/app/form/create-die-set/create-die-set.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { stringify } from 'querystring';
+import { formatCurrency } from '@angular/common';
+//import { AppComponent } from './app.component'
+import {NgForm} from '@angular/forms';
 
 @Component({
   selector: 'app-create-die-set',
@@ -6,10 +10,27 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-die-set.component.css']
 })
 export class CreateDieSetComponent implements OnInit {
-
+  dieTypes: any = [
+    '4',
+    '6',
+    '8',
+    '10',
+    '12',
+    '20'
+  ]
   constructor() { }
 
   ngOnInit() {
   }
 
+  onSubmit(form: NgForm) {
+    console.log('Your form data : ', form.value);
+    let rollValue = form.value.rollValue;
+    let selectedDieType = form.value.dieType;
+    let existing = localStorage.getItem(`${selectedDieType}`);
+    let existingArray = existing ? existing.split(',') : [];
+    existingArray.push(rollValue);
+    localStorage.setItem(selectedDieType, existingArray.toString());
+    form.reset();
+  }
 }
