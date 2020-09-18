5c91493aafe7da89e1a3de2bfcd379aa2a8b50d9
Caroline STone <docphilstone@gmail.com>
2020-09-18 14:28:21 -0500

    multidice produces repeated dice roller

--- src/app/multi-dice-roller/multi-dice-roller.component.ts
+++ src/app/multi-dice-roller/multi-dice-roller.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, NgModule } from '@angular/core';
 import { DiceRollerComponent } from '../dice-roller/dice-roller.component';
 import { BrowserModule } from '@angular/platform-browser';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { FormBuilder } from '@angular/forms';
 
 @NgModule({
   declarations: [
@@ -21,15 +22,36 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
 export class MultiDiceRollerComponent implements OnInit {
 
   multi = false;
+  multiSelected = false;
   numberDie = 0;
+  Arr = Array;
+  num:number = 2;
+  numbers: any[] = [];
 
-  constructor() { }
+  constructor(public fb:FormBuilder) { }
 
   ngOnInit() {
+    this.populateNumbers(10);
+    console.log(this.numbers);
   }
+
+  dieNumberForm = this.fb.group({
+    name: ['']
+  })
 
   multiRoll() {
     this.multi = true;
+  }
+
+  populateNumbers(upperLimit) {
+    for (let i = 0; i <= upperLimit; i ++) {
+      this.numbers.push(i);
+    }
+  }
+
+  dieNumberSubmit() {
+    this.num=Number(this.dieNumberForm.value['name']);
+    this.multiSelected = true;
   }
 
 }
