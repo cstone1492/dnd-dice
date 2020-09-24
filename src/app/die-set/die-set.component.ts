955fa90d48d5d9c0d5a99cde734cdbd3ccdcf824
Caroline STone <docphilstone@gmail.com>
2020-09-24 16:46:10 -0500

    added delete function

--- src/app/die-set/die-set.component.ts
+++ src/app/die-set/die-set.component.ts
@@ -16,6 +16,19 @@ export class DieSetComponent implements OnInit {
     console.log(this.dieSets)
   }
 
+  deleteDieSet(index) {
+    this.dieSets.splice(index, 1);
+    let dieSetsJSON = "";
+    for (let dieSet of this.dieSets) {
+      if (this.dieSets.indexOf(dieSet) == 0) {
+        dieSetsJSON = JSON.stringify(dieSet);
+      } else {
+        dieSetsJSON = dieSetsJSON + ";" + JSON.stringify(dieSet);
+      }
+    }
+    localStorage.setItem( "dieSets", dieSetsJSON);
+  }
+
   constructor() { }
 
   ngOnInit() {
